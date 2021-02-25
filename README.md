0aed73a0c5419997c705b36697b2e80f6f3f8af5
Omar Trasatti <omar@softintigate.com>
2021-02-25 17:46:29 +0100

    Add commons-email dependency, implement simple mail send

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # README
 
-ErmesMail è un insieme di classi Java per inviare messaggi email tramite ... .
+ErmesMail è un insieme di classi Java per inviare messaggi email tramite server SMTP .
+
+### SimpleMessage
+
+1. build dell'applicazione: `mvn install`
+2. avviare l'applicazione passando i seguenti parametri.
+
+
+*NB: Per inviare messaggi tramite SMTP Google è necessario configurare l'account gmail abilitando IMAP. [Maggiori informazioni](https://support.google.com/mail/answer/7126229)*
+
+### Troubleshooting
+
+`javax.mail.AuthenticationFailedException: 535-5.7.8 Username and Password not accepted.` [Soluzione] (https://stackoverflow.com/questions/43406528/javamail-api-username-and-password-not-accepted-gmail)
+
+
--- src/main/java/com/softinstigate/ermes/mail/Sender.java
+++ src/main/java/com/softinstigate/ermes/mail/Sender.java
@@ -1,12 +1,34 @@
 package com.softinstigate.ermes.mail;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 public class Sender {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EmailException {
         String from = args[0];
         String to = args[1];
-        String message = args[2];
+        String object = args[2];
+        String message = args[3];
+        String hostName = args[4];
+        int smtpPort = Integer.parseInt(args[5]);
+        String username = args[6];
+        String password = args[7];
 
         System.out.format("from='%s',  to='%s', message='%s'\n", from, to, message);
+
+        var email = new SimpleEmail();
+
+        email.setHostName(hostName);
+        email.setSmtpPort(smtpPort);
+        email.setAuthenticator(new DefaultAuthenticator(username, password));
+        email.setSSLOnConnect(true);
+        email.setFrom(from);
+        email.setSubject(object);
+        email.setMsg(message);
+        email.addTo(to);
+        email.send();
     }
 }
